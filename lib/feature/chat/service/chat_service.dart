49831aa5532c0419813d62506686fbec97d6753f
Nayma Amin <naymaaminnishy06@gmail.com>
2026-03-03 14:40:18 +0600

    Chat updated

--- lib/feature/chat/service/chat_service.dart
+++ lib/feature/chat/service/chat_service.dart
@@ -6,21 +6,26 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebSocketService {
   WebSocketChannel? _channel;
+  StreamSubscription? _subscription;
+
+  final StreamController<String> _messageController =
+      StreamController<String>.broadcast();
 
   final isConnected = false.obs;
   final isAuthenticated = false.obs;
 
-  StreamSubscription? _subscription;
+  Stream<String> get messages => _messageController.stream;
 
   void connect(String url, String token) {
     try {
       _channel = WebSocketChannel.connect(Uri.parse(url));
-
       isConnected.value = true;
 
+      _subscription?.cancel();
       _subscription = _channel!.stream.listen(
         (message) {
           _handleIncoming(message);
+          _messageController.add(message);
         },
         onDone: () {
           if (kDebugMode) print("WebSocket closed.");
@@ -43,18 +48,13 @@ class WebSocketService {
 
   void _authenticate(String token) {
     final authMessage = jsonEncode({"event": "authenticate", "token": token});
-
     _channel?.sink.add(authMessage);
-
-    if (kDebugMode) {
-      print("Sent authentication message: $authMessage");
-    }
+    if (kDebugMode) print("Sent auth: $authMessage");
   }
 
   void _handleIncoming(dynamic message) {
     try {
       final data = jsonDecode(message);
-
       if (data['event'] == 'authenticated') {
         if (kDebugMode) print("WebSocket authenticated.");
         isAuthenticated.value = true;
@@ -62,24 +62,17 @@ class WebSocketService {
     } catch (_) {}
   }
 
-  Stream get messages => _channel!.stream;
-
   void sendMessage(String event, Map<String, dynamic> data) {
     if (!isConnected.value) {
       if (kDebugMode) print("Cannot send. Socket not connected.");
       return;
     }
-
     final message = jsonEncode({"event": event, ...data});
     _channel?.sink.add(message);
-
-    if (kDebugMode) {
-      print("Sent WebSocket message: $message");
-    }
+    if (kDebugMode) print("Sent: $message");
   }
 
   void close() {
-    if (kDebugMode) print("Closing WebSocket connection.");
     isConnected.value = false;
     isAuthenticated.value = false;
     _subscription?.cancel();
